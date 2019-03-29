c5fbb9475fb7ec6d47614a3d837e150fb95a9789
schumak <sergeychumakru@gmail.com>
2019-03-29 08:29:04 +0300

    init

--- razbor18/src/components/HelloWorld.vue
+++ razbor18/src/components/HelloWorld.vue
@@ -1,33 +1,55 @@
 <template>
- <div>
-   909
-    {{asd}}
-     <button @click="inc">asd</button>
- </div>
+    <div>
+        909
+        <!--{{books}}-->
+        <!--<article v-for="(location, idx) in locations" :key="idx">-->
+            <!--<img :src="location.image" width="300px">-->
+            <!--<h1>{{ location.name }}</h1>-->
+            <!--<button class="button is-small is-danger" @click="deleteLocation(location.id)">-->
+                <!--Delete-->
+            <!--</button>-->
+        <!--</article>-->
+
+        <!--<hr>-->
+
+        <!--<form @submit="addLocation(name, image)">-->
+            <!--<h2>Add a New Location</h2>-->
+            <!--<input v-model="name" placeholder="Location Name" class="input">-->
+            <!--<input v-model="image" placeholder="Location Image URL" class="input">-->
+            <!--<button type="submit" class="button is-success">Add New Location</button>-->
+        <!--</form>-->
+    </div>
 </template>
 
 <script>
-    import {mapGetters, mapActions} from 'vuex'
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-    mounted: function(){
-        console.log(11)
-        console.log(this.$store.getters.asd)
-        console.log(this.$store.getters)
-    },
-    methods: {
-        ...mapActions('TestASD', {
-            inc: 'inc'
-        })
-    },
-    computed: {
-        ...mapGetters('TestASD', {
-            asd: "count"
-        })
-    }
-}
-</script>
 
+    // import { db, asd } from '../main'
+    export default {
+        name: 'HelloWorld',
+        data () {
+            return {
+                locations: [],
+                name: '',
+                image: ''
+            }
+        },
+        // firebase () {
+        //     return {
+        //         books: asd
+        //     }
+        // },
+        // firestore () {
+        //     return {
+        //         locations: db.collection('locations')
+        //     }
+        // },
+        methods: {
+            // addLocation (name, image) {
+            //     db.collection('locations').add({ name, image })
+            // },
+            // deleteLocation (id) {
+            //     db.collection('locations').doc(id).delete()
+            // }
+        }
+    }
+</script>
--- razbor18/src/main.js
+++ razbor18/src/main.js
@@ -2,6 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/database'
+Vue.use(VueFire)
+
+import firebaseConfig from './firebase_config'
+firebase.initializeApp(firebaseConfig)
+const firestore = firebase.firestore()
+const settings = { }
+firestore.settings(settings)
+
+let db1 = firebase.database()
+let booksRef = db1.ref('locations')
+
+export const db = firestore
+export const asd = booksRef
+
+
 Vue.config.productionTip = false
 
 new Vue({
